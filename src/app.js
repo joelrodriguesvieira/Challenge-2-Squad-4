1c34618f0c21ba1466bb636503ccf4b4f7e6b9f1
João Henrique Ferreira <157930541+J-Ferre1ra@users.noreply.github.com>
2024-04-11 20:40:28 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const connectDb = require('../infra/db');
+const movieRoutes = require('../src/routes/movie.routes');
+const sessionRoutes = require('../src/routes/session.routes');
+const ticketsRoutes = require('../src/routes/tickets.routes');
+
+require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 3000;
 
